98ea4b3c86f137f810b36469b7793abf2242077d
Arian Derida <arianderida@gmail.com>
2020-06-08 18:43:55 +0700

    simple roll dice kotlin

---
+++ app/src/main/java/com/derida/rolldice/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.derida.rolldice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+
+        rollButton.setOnClickListener{
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val diceImage: ImageView = findViewById(R.id.image_dice)
+        val drawableRes = when(Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableRes)
+    }
+}
